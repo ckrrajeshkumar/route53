2d7fb55b8fee0b460aea990a632d009ec36e3a4d
ckrrajeshkumar <ckrrajeshkumar@gmail.com>
2025-07-03 00:30:25 -0400

    hghgh

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -4,9 +4,8 @@ variable "pubcidr1"{}
 variable "pubcidr2"{}
 variable "pubsubname1"{}
 variable "pubsubname2"{}
-varaible "pubaz2"{}
-variable "pubaz1"{}
 variable "pubaz2"{}
+variable "pubaz1"{}
 variable "igwname"{}
 variable "igwcidr"{}
 variable "rtname"{}
